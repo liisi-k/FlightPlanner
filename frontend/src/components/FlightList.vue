a33d0ae209b0f2f85ce27d37a2d62609cce6c2e8
likask <likask@taltech.ee>
2025-03-17 19:34:21 +0200

    Add style to FlightList

--- frontend/src/components/FlightList.vue
+++ frontend/src/components/FlightList.vue
@@ -1,12 +1,16 @@
 <template>
+    <div class="allBox">
       <h2>Available Flights</h2>
       <ul v-if="flights.length > 0">
         <li v-for="flight in flights" :key="flight.flightNumber">
-              <span class="text">{{ flight.destination }}</span> | 
-              <span class="text">{{ flight.price }}</span> | 
-              <span class="text">{{ flight.departureDate }}</span>
+          <div class="flight-box">
+              <span class="destination">{{ flight.destination }}</span> 
+              <span class="price">{{ flight.price }}</span> 
+              <span class="departure-date">{{ flight.departureDate }}</span>
+          </div>
         </li>
       </ul>
+    </div>
   </template>
   
   <script>
@@ -16,4 +20,56 @@
     }
   };
   </script>
+  
+  <style scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  li {
+    margin-bottom: 15px;
+  }
+  
+  .allBox {
+    padding: 30px;
+    border: 2px solid #f1f1f1;
+    background-color: rgba(0, 0, 0, 0.05);
+    border-radius: 12px;
+    color: #333;
+    max-width: 800px;
+  }
+
+  .flight-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    background-color: #f7faff;
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+  }
+  
+  .destination {
+    font-weight: bold;
+    color: #333;
+    font-size: 18px;
+    width: 90px;
+  }
+  
+  .price {
+    font-weight: bold;
+    color: #007bff;
+    font-size: 20px;
+    text-align: center;  
+}
+  
+  .departure-date {
+    color: #888;
+    font-size: 14px;
+    text-align: right;
+  }
+  </style>
   
