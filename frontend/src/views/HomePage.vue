301e92645b2b754913432f34c2e0f6f1ec9e7447
likask <likask@taltech.ee>
2025-03-17 19:45:30 +0200

    Add filtering to search flights by destination

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <input
+      v-model="destination"
+      @input="filterFlights"
+      @keyup.enter="searchFlights"
+      placeholder="Enter your destination"
+      class="destinationSearch"
+    />
     <FlightList :flights="filteredFlights" />
   </div>
 </template>
@@ -30,6 +37,32 @@ export default {
         console.error('Error fetching flights:', error);
       }
     },
+    filterFlights() {
+      this.filteredFlights = this.flights.filter(flight =>
+        flight.destination.toLowerCase().includes(this.destination.toLowerCase())
+      );
+    },
+    async searchFlights() {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/flights?destination=${this.destination}`);
+        this.filteredFlights = response.data;
+      } catch (error) {
+        console.error('Error fetching flights:', error);
+      }
+    }
   }
 };
 </script>
+
+<style scoped>
+.destinationSearch {
+  margin-bottom: 20px;
+  background-color: rgb(255, 255, 255);
+  padding: 8px;
+  border-radius: 10px;
+  width: 250px;
+  height: 30px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+}
+</style>
